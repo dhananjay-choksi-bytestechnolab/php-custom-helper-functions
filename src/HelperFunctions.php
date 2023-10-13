e480c376967d8455b7a0660e46f4876da9a4e8e3
taral.bytes <taral.nagar@bytestechnolab>
2023-10-13 15:25:30 +0530

    Added changes

---
+++ src/HelperFunctions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BytesDhananjay\HelperFunctions;
+
+class HelperFunctions
+{
+    public static function sayHello()
+    {
+        return "Hello from HelperFunctions!";
+    }
+
+    // Add more helper functions as needed
+}
